ee0dcfea9afd612db859ea167eb2c508336132f3
0x00a <paolo@async.ly>
2018-07-31 14:12:02 +0200

    fix incorrect param as old props

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ class Tonic extends window.HTMLElement {
   }
 
   setProps (o) {
-    const oldProps = JSON.parse(JSON.stringify(o))
+    const oldProps = JSON.parse(JSON.stringify(this.props))
     this.props = Tonic.sanitize(typeof o === 'function' ? o(this.props) : o)
     if (!this.root) throw new Error('Component not yet connected')
     this.root.appendChild(this._setContent(this.render()))
