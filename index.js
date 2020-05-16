9ad8dc410bbe9f39089c861ad75e98c109cb82ed
heapwolf <paolo@async.ly>
2020-05-16 14:49:18 +0200

    provide a way for nonce to be passed to stylesheets, satisfy csp

--- index.js
+++ index.js
@@ -111,6 +111,7 @@ class Tonic extends window.HTMLElement {
     Tonic._stylesheetRegistry.push(stylesheetFn)
 
     const styleNode = document.createElement('style')
+    if (Tonic.nonce) styleNode.setAttribute('nonce', Tonic.nonce)
     styleNode.appendChild(document.createTextNode(stylesheetFn()))
     if (document.head) document.head.appendChild(styleNode)
   }
@@ -254,7 +255,7 @@ class Tonic extends window.HTMLElement {
 
     if (typeof content === 'string') {
       if (this.stylesheet) {
-        content = `<style>${this.stylesheet()}</style>${content}`
+        content = `<style ${Tonic.nonce || ''}>${this.stylesheet()}</style>${content}`
       }
 
       target.innerHTML = content
