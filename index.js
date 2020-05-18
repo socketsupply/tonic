416d54fa224c066c4e06e5608caeaacd03fb7a5f
Raynos <raynos2@gmail.com>
2020-05-18 11:00:16 +0200

    src: Do not use `.id` in the Tonic class itself
    Here when we want to access the id without triggering the
    mandatory id check we use `super.id` .
    
    In another part of the code where we want to access the id
    only if exists, instead of using `node.id` we use
    `node.getAttribute('id')` which we have not overwritten.

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ class TonicRaw {
 class Tonic extends window.HTMLElement {
   constructor () {
     super()
-    const state = Tonic._states[this.id]
-    delete Tonic._states[this.id]
-    this.state = state || {}
+    const state = Tonic._states[super.id]
+    delete Tonic._states[super.id]
+    this._state = state || {}
     this.props = {}
     this.elements = [...this.children]
     this.elements.__children__ = true
@@ -253,8 +253,10 @@ class Tonic extends window.HTMLElement {
   _set (target, render, content = '') {
     for (const node of target.querySelectorAll(Tonic._tags)) {
       if (!node.isTonicComponent) continue
-      if (!node.id || !Tonic._refIds.includes(node.id)) continue
-      Tonic._states[node.id] = node.getState()
+
+      const id = node.getAttribute('id')
+      if (!id || !Tonic._refIds.includes(id)) continue
+      Tonic._states[id] = node.getState()
     }
 
     if (render instanceof Tonic.AsyncFunction) {
@@ -299,7 +301,7 @@ class Tonic extends window.HTMLElement {
       const walk = (node, fn) => {
         if (node.nodeType === 3) {
           const id = node.textContent.trim()
-          if (children[id]) fn(node, children[id])
+          if (children[id]) fn(node, children[id], id)
         }
 
         const childNodes = node.childNodes
@@ -310,11 +312,11 @@ class Tonic extends window.HTMLElement {
         }
       }
 
-      walk(target, (node, children) => {
+      walk(target, (node, children, id) => {
         for (const child of children) {
           node.parentNode.insertBefore(child, node)
         }
-        delete Tonic._children[this._id][node.id]
+        delete Tonic._children[this._id][id]
         node.parentNode.removeChild(node)
       })
     } else {
@@ -331,8 +333,8 @@ class Tonic extends window.HTMLElement {
       this.render = this.wrap
     }
 
-    if (this.id && !Tonic._refIds.includes(this.id)) {
-      Tonic._refIds.push(this.id)
+    if (super.id && !Tonic._refIds.includes(super.id)) {
+      Tonic._refIds.push(super.id)
     }
     const cc = s => s.replace(/-(.)/g, (_, m) => m.toUpperCase())
 
