72f5d44a8a5cffc58aeaee249ed09fc0de2e3d20
Steve Ruiz <steveruizok@gmail.com>
2021-10-23 21:45:33 +0100

    untrack node modules from example

--- example/node_modules/date-fns/esm/fp/previousSaturday/index.js
+++
@@ -1,5 +0,0 @@
-// This file is generated automatically by `scripts/build/fp.js`. Please, don't change it.
-import fn from "../../previousSaturday/index.js";
-import convertToFP from "../_lib/convertToFP/index.js";
-var previousSaturday = convertToFP(fn, 1);
-export default previousSaturday;
